3433cdba0c568a3c1aa2ff416061ce9dc288c889
Navya392 <navyachary95@gmail.com>
2024-12-13 19:14:07 -0500

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HomeSection/>
+  </div>
+</template>
+
+<script>
+import HomeSection from './components/HomeSection.vue';
+
+export default {
+  data() { 
+  },
+  methods: {
+  },
+  name: 'App',
+  components :  {
+    HomeSection
+  }
+};
+</script>
+
+<style>
+
+</style>
